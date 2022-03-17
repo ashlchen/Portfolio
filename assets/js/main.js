021125c375a242b96f5204ddef62249a30fc49ad
ashlchen <ashlchen@iu.edu>
2022-03-17 13:05:12 -0400

    Add graduation countdown clock in the about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,3 +136,41 @@ function scrollTop(){
   window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
+
+/*==================== Countdown ====================*/
+
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minsE1 = document.getElementById('mins');
+const secondsE1  = document.getElementById('seconds');
+
+const Graduation = '16 Dec 2022'
+
+function countdown() {
+    const currentDate = new Date();
+    const GraduationDate = new Date(Graduation);
+
+    const totalseconds = (GraduationDate - currentDate) / 1000;
+    const days = Math.floor(totalseconds / 3600/ 24);
+    const hours = Math.floor(totalseconds/3600) % 24;
+    const mins = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor(totalseconds % 60);
+    
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = formatTime(hours);
+    minsE1.innerHTML = formatTime(mins);
+    secondsE1.innerHTML = formatTime(seconds);
+
+    
+
+
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// Initial call
+countdown();
+
+setInterval(countdown, 1000)
